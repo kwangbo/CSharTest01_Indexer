323499c2119bf2e549c367887df17835590bfcee
kwangbo <giggle_@naver.com>
2020-11-06 14:45:55 +0900

    확장메서드

---
+++ ExtensionDemo/ExtensionDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyExtensions
+{
+
+    static class StringExtension
+    {
+
+        public static string Ten(this string msg)
+        {
+            return msg.Substring(0, 10);
+        }
+
+        public static string Five(this string msg) 
+        { 
+            return msg.Substring(0, 5); 
+        }
+
+        public static string AddElipsis(this string msg)
+        {
+            return msg + "..."; 
+        }
+        public static string AddElipsis(this string msg, string elipsis)
+        {
+            return msg + elipsis;
+        }
+
+    }
+    class ExtensionDemo
+    {
+        static void Main()
+        {
+            string message = "안녕하세요 반값습니다. 또만나요, 즐거운 주말 되세요";
+            Console.WriteLine(message.Ten().Five().AddElipsis());
+            Console.WriteLine(message.Ten().Five().AddElipsis("_____"));
+        }
+    }
+}
---
+++ ExtensionsTest1/ExtentionsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtensionsTest1
+{
+    static class StringExtension
+    {
+
+    }
+
+    class MyExtenstionsDemo
+    {
+        static void Main()
+        {
+            string message = "안녕하세요. 반갑습니다. 또 만나요.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
